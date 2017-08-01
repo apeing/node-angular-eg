444c35db31340a135c63106d1d06b97cfbe339a4
lxl <562578014@qq.com>
2017-08-01 17:36:25 +0800

    构建基础结构

---
+++ public/blog/scripts/app.js
@@ -0,0 +1,16 @@
+﻿(function(angular){
+    'use strict';
+    angular.module('mainblog',['ngRoute'])
+        .config(['$routeProvider', function ($routeProvider) {
+            $routeProvider.when('/t1', {
+                templateUrl: 'view/1.html',
+				controller: 't1Controller'
+            }).when('/t2', {
+				templateUrl: 'view/2.html'
+			}).otherwise({
+                redirectTo: '/'
+            });
+        }]).run(['$rootScope', '$window', function($rootScope, $window) {
+
+    }]);
+})(angular);
---
+++ public/blog/scripts/controller/t1controller.js
@@ -0,0 +1,10 @@
+﻿(function(angular){
+    'use strict';
+    angular.module('mainblog').controller('t1Controller',
+        ['$scope', '$window', function($scope, $window){
+            function init(){
+                console.log("local : " + $window.location.href);
+            }
+            init();
+        }]);
+})(angular);
